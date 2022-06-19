2f978dcf3bb36e51bc517b523fb50de94942a3a2
Vinicius Alves <vinicius_o.a@live.com>
2022-06-19 17:47:56 -0300

    just trolling the repo

---
+++ php/fundamentos/arithmetic/float-truncation.php
@@ -0,0 +1,8 @@
+<?php
+
+$x = 10.5;
+
+echo "(int) {$x}  = " . (int) $x  . PHP_EOL;
+echo "round({$x}) = " . round($x) . PHP_EOL;
+echo "ceil({$x})  = " . ceil($x)  . PHP_EOL;
+echo "floor({$x}) = " . floor($x) . PHP_EOL;
---
+++ php/fundamentos/if/ternario.php
@@ -0,0 +1,6 @@
+<?php
+
+$idade = 18;
+$maior = 18;
+
+echo ($idade >= $maior) ? "Maior de Idade" : "Menor de Idade";
