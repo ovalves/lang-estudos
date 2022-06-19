81ce2af71c1d42c1ce66125572bf0d602b4486d3
Vinicius Alves <cit.vinicius@unidas.com.br>
2022-06-19 16:47:55 -0300

    Testing in progress ;)

---
+++ php/fundamentos/foreach/foreach.php
@@ -0,0 +1,20 @@
+<?php
+
+$meses = [
+    1 => "Janeiro",
+    2 => "Fevereiro",
+    3 => "Março",
+    4 => "Abril",
+    5 => "Maio",
+    6 => "Junho",
+    7 => "Julho",
+    8 => "Agosto",
+    9 => "Setembro",
+    10 => "Outubro",
+    11 => "Novembro",
+    12 => "Dezembro"
+];
+
+foreach ($meses as $index => $mes) {
+    echo "Mês: {$index} - {$mes}\n";
+}
