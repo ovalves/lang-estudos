422d36964ceec34fe15bb73542b27a69f78f7b6c
Vinicius Alves <vinicius_o.a@live.com>
2022-06-19 18:26:59 -0300

    No changes after this point.

---
+++ php/fundamentos/strings/format-string.php
@@ -0,0 +1,4 @@
+<?php
+
+$fmt = "Formatando a string com: STRING: %s, INT: %d, FLOAT: %f";
+echo sprintf($fmt, "texto qualquer", 10, 7.5) . PHP_EOL; // Formatando a string com: STRING: texto qualquer, INT: 10, FLOAT: 7.500000
---
+++ php/fundamentos/strings/trim.php
@@ -0,0 +1,5 @@
+<?php
+
+echo trim(" lorem ") . PHP_EOL; // lorem
+echo ltrim(" lorem") . PHP_EOL; // lorem
+echo rtrim("lorem ") . PHP_EOL; // lorem
