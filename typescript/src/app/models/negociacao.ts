853dac41ef499a3b9e0d3c3a9981c6369dab9c42
Vinicius Alves <cit.vinicius@locamerica.tec.br>
2022-08-19 02:12:15 -0300

    ...

--- typescript/src/app/models/negociacao.ts
+++ typescript/src/app/models/negociacao.ts
@@ -1,13 +1,28 @@
 export class Negociacao {
-    constructor(private _data: Date, public readonly quantidade: number, public readonly valor: number) {
-        //
-    }
+    constructor(
+        private _data: Date,
+        public readonly quantidade: number,
+        public readonly valor: number
+    ) {}
 
     get volume(): number {
         return this.quantidade * this.valor;
     }
 
     get data(): Date {
-        return new Date(this._data.getTime());
+        const data = new Date(this._data.getTime());
+        return data;
+    }
+
+    public static criaDe(
+        dataString: string,
+        quantidadeString: string,
+        valorString: string
+    ): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(date, quantidade, valor);
     }
 }
